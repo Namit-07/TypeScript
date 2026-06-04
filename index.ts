766a4e19eb2696198bb1ba6dd72a99f380408357
Namit-07 <namittickoo2006@gmail.com>
2026-06-04 21:36:01 +0530

    Protected

--- index.ts
+++ index.ts
@@ -13,13 +13,44 @@
 
 
 class Userr {
+
+    private _courseCount = 1
+
     readonly city: string = "Delhi"
     constructor(
         public email: string,
         name: string,
         private userId: number
     ) {
+    }
+    private deleteToken(){
+        console.log("Token deleted")
+    }
 
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount    
+    }
+
+    set courseCount(courseNum) {    
+        if(courseNum <= 1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+
+}
+
+class SubUser extends Userr{
+    isFamily: boolean = true
+    changeCourseCount(){
+        this.courseCount = 4
     }
 }
+
+const namitt = new Userr("namit77@gmail.com", "Namit", 1)
+namitt.deleteToken() // Error: Property 'deleteToken' is private and only accessible within class 'Userr'.
 
