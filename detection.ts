647b2ea76c431d5c6b4a77a53fb332508f837a9d
Namit-07 <namittickoo2006@gmail.com>
2026-06-18 21:52:29 +0530

    Discrimated union and exhaustive checking

--- detection.ts
+++ detection.ts
@@ -52,3 +52,46 @@ function getFood(pet: Fish | Bird){
         return "bird food";
     }
 }
+
+
+interface Circle {
+    kind: "circle",
+    radius: number
+}
+
+interface Square {
+    kind: "square"
+    side: number
+}
+
+interface Rectangle {
+    kind: "rectangle"
+    length: number
+    width: number
+}
+
+type Shape = Circle | Square | Rectangle
+
+function getTrueShape(shape: Shape){
+    if(shape.kind === "circle"){ // discriminated union that uses a common property to narrow down the type of a variable
+        return Math.PI * shape.radius ** 2;
+    }
+    return shape.side * shape.side; 
+}
+
+function getArea(shape: Shape){
+    switch(shape.kind){ // switch statement that uses a discriminated union to narrow down the type of a variable
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        
+        case "square":
+            return shape.side * shape.side; 
+
+        case "rectangle":
+            return shape.length * shape.width;
+
+        default:
+            const _defaultForShape: never = shape; // never type that represents a value that never occurs
+            return _defaultForShape;
+    }
+}
